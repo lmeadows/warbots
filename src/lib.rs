9caf47b6fb3aad722d6ad7db82565d0c593490a0
lmeadows <lmeadows@vmware.com>
2020-01-11 19:52:18 -0700

    Moving terrain generation from JS to Rust. Still need to get randomized terrain color working

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,85 @@
 mod utils;
 
+use std::f64;
 use wasm_bindgen::prelude::*;
+use wasm_bindgen::JsCast;
 
-// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-// allocator.
-#[cfg(feature = "wee_alloc")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+#[wasm_bindgen]
+extern "C" {
+    // Use `js_namespace` here to bind `console.log(..)` instead of just
+    // `log(..)`
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn start() {
+    let document = web_sys::window().unwrap().document().unwrap();
+    let canvas = document.get_element_by_id("warbots-canvas").unwrap();
+    let canvas: web_sys::HtmlCanvasElement = canvas
+        .dyn_into::<web_sys::HtmlCanvasElement>()
+        .map_err(|_| ())
+        .unwrap();
+
+    let context = canvas
+        .get_context("2d")
+        .unwrap()
+        .unwrap()
+        .dyn_into::<web_sys::CanvasRenderingContext2d>()
+        .unwrap();
+
+    draw_terrain(&context);
+}
+
+use rand::Rng;
+pub fn draw_terrain(context: &web_sys::CanvasRenderingContext2d) {
+    const STEP_MAX: f64 = 2.5;
+    const STEP_CHANGE: f64 = 1.0;
+    const HEIGHT_MAX: f64 = 500.0;
+
+    let mut rng = rand::thread_rng();
+
+    // starting conditions
+    let y1: f64 = rng.gen();
+    let mut terrain_height: f64 = y1 * HEIGHT_MAX;
+    let y2: f64 = rng.gen();
+    let mut slope: f64 = (y2 * STEP_MAX) * 2.0 - STEP_MAX;
+
+    let c = JsValue::from(String::from("#0000FF"));
+    context.set_stroke_style(&c);
+
+    // create the landscape
+    for x in 0..900 {
+        // change height and slope
+        terrain_height += slope;
+        let y3: f64 = rng.gen();
+        slope += (y3 * STEP_CHANGE) * 2.0 - STEP_CHANGE;
+
+        // clip height and slope to maximum
+        if slope > STEP_MAX {
+            slope = STEP_MAX;
+        }
+
+        if slope < -1.0 * STEP_MAX {
+            slope = -1.0 * STEP_MAX;
+        }
+
+        if terrain_height > HEIGHT_MAX {
+            terrain_height = HEIGHT_MAX;
+            slope *= -1.0;
+        }
+
+        if terrain_height < 0.0 {
+            terrain_height = 0.0;
+            slope *= -1.0;
+        }
+        // draw column
+        context.begin_path();
+        context.move_to(x as f64, HEIGHT_MAX);
+        context.line_to(x as f64, terrain_height);
+        context.stroke();
+    }
+}
 
 #[wasm_bindgen]
 pub struct Config {
--- www/index.js
+++ www/index.js
@@ -1,10 +1,12 @@
 import "./canvas.css"
-import { Config } from "warbots";
+import { Config, start } from "warbots";
 
 const config = Config.new();
 const canvas = document.getElementById("warbots-canvas");
 canvas.height = config.height();
 canvas.width = config.width();
+
+start();
 
 const TERRAIN_COLORS = [
   "27FF00",
@@ -16,58 +18,3 @@ const TERRAIN_COLORS = [
   "FFFFFF",
   "BAEFFF"
 ]
-
-const TERRAIN_COLOR = "#" + TERRAIN_COLORS[Math.floor(Math.random() * TERRAIN_COLORS.length)]
-const CELL_SIZE = 5;
-const ctx = canvas.getContext('2d');
-const height = 5;
-const width = 5;
-
-ctx.strokeStyle = TERRAIN_COLOR;
-
-// parameters - change to your liking
- var STEP_MAX = 2.5;
- var STEP_CHANGE = 1.0;
- var HEIGHT_MAX = canvas.height;
-
- // starting conditions
- var terrain_height = Math.random() * HEIGHT_MAX;
- var slope = (Math.random() * STEP_MAX) * 2 - STEP_MAX;
-
- // creating the landscape
- for (var x = 0; x < canvas.width; x++) {
-      // change height and slope
-      terrain_height += slope;
-      slope += (Math.random() * STEP_CHANGE) * 2 - STEP_CHANGE;
-
-      // clip height and slope to maximum
-      if (slope > STEP_MAX) { slope = STEP_MAX };
-      if (slope < -STEP_MAX) { slope = -STEP_MAX };
- 
-      if (terrain_height > HEIGHT_MAX) { 
-          terrain_height = HEIGHT_MAX;
-          slope *= -1;
-      }
-      if (terrain_height < 0) { 
-          terrain_height = 0;
-          slope *= -1;
-      }
-      
-      // draw column
-      ctx.beginPath();
-      ctx.moveTo(x, HEIGHT_MAX);
-      ctx.lineTo(x, terrain_height);
-      ctx.stroke();
- }
-
-
-
-const renderLoop = () => {
-  ctx.fillStyle="red";
-  ctx.fillRect(20, 20, 10, 10);
-  ctx.fillStyle="blue";
-  ctx.fillRect(880, 20, 10, 10);
-	requestAnimationFrame(renderLoop);
-};
-
-requestAnimationFrame(renderLoop);
