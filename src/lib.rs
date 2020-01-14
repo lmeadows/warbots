65e8a17050b9643ce6e82faeb9ed972a860ab4fb
lmeadows <lmeadows@vmware.com>
2020-01-14 11:14:16 -0700

    Figured out how to create and track state outside of the rendering logic

--- src/lib.rs
+++ src/lib.rs
@@ -14,6 +14,8 @@ extern "C" {
 
 #[wasm_bindgen]
 pub fn start() {
+    let mut heights: [f64; 900] = [0f64; 900];
+    let terrain: Terrain = Terrain::new(heights);
     let document = web_sys::window().unwrap().document().unwrap();
     let canvas = document.get_element_by_id("warbots-canvas").unwrap();
     let canvas: web_sys::HtmlCanvasElement = canvas
@@ -28,56 +30,78 @@ pub fn start() {
         .dyn_into::<web_sys::CanvasRenderingContext2d>()
         .unwrap();
 
-    draw_terrain(&context);
+    /*
+     Not sure how to get this to work
+    let terrain: &'static Terrain = Terrain::new();
+    let heights: [f64; 900] = terrain.heights();
+    */
+    draw_terrain(&context, terrain.heights());
 }
 
 use rand::Rng;
-pub fn draw_terrain(context: &web_sys::CanvasRenderingContext2d) {
-    const STEP_MAX: f64 = 2.5;
-    const STEP_CHANGE: f64 = 1.0;
-    const HEIGHT_MAX: f64 = 500.0;
-
-    let mut rng = rand::thread_rng();
-
-    // starting conditions
-    let y1: f64 = rng.gen();
-    let mut terrain_height: f64 = y1 * HEIGHT_MAX;
-    let y2: f64 = rng.gen();
-    let mut slope: f64 = (y2 * STEP_MAX) * 2.0 - STEP_MAX;
-
+pub fn draw_terrain(context: &web_sys::CanvasRenderingContext2d, heights: [f64; 900]) {
     let c = JsValue::from(String::from("#0000FF"));
     context.set_stroke_style(&c);
 
-    // create the landscape
     for x in 0..900 {
-        // change height and slope
-        terrain_height += slope;
-        let y3: f64 = rng.gen();
-        slope += (y3 * STEP_CHANGE) * 2.0 - STEP_CHANGE;
-
-        // clip height and slope to maximum
-        if slope > STEP_MAX {
-            slope = STEP_MAX;
-        }
-
-        if slope < -1.0 * STEP_MAX {
-            slope = -1.0 * STEP_MAX;
-        }
-
-        if terrain_height > HEIGHT_MAX {
-            terrain_height = HEIGHT_MAX;
-            slope *= -1.0;
-        }
-
-        if terrain_height < 0.0 {
-            terrain_height = 0.0;
-            slope *= -1.0;
-        }
-        // draw column
         context.begin_path();
-        context.move_to(x as f64, HEIGHT_MAX);
-        context.line_to(x as f64, terrain_height);
+        context.move_to(x as f64, 500.0);
+        context.line_to(x as f64, heights[x]);
         context.stroke();
+    }
+}
+
+struct Terrain {
+    heights: [f64; 900],
+}
+
+impl Terrain {
+    fn new(mut heights: [f64; 900]) -> Terrain {
+        const STEP_MAX: f64 = 2.5;
+        const STEP_CHANGE: f64 = 1.0;
+        const HEIGHT_MAX: f64 = 450.0;
+
+        let mut rng = rand::thread_rng();
+
+        // starting conditions
+        let y1: f64 = rng.gen();
+        let mut terrain_height: f64 = y1 * HEIGHT_MAX;
+        let y2: f64 = rng.gen();
+        let mut slope: f64 = (y2 * STEP_MAX) * 2.0 - STEP_MAX;
+
+        // create the landscape
+        for x in 0..900 {
+            // change height and slope
+            terrain_height += slope;
+            let y3: f64 = rng.gen();
+            slope += (y3 * STEP_CHANGE) * 2.0 - STEP_CHANGE;
+
+            // clip height and slope to maximum
+            if slope > STEP_MAX {
+                slope = STEP_MAX;
+            }
+
+            if slope < -1.0 * STEP_MAX {
+                slope = -1.0 * STEP_MAX;
+            }
+
+            if terrain_height > HEIGHT_MAX {
+                terrain_height = HEIGHT_MAX;
+                slope *= -1.0;
+            }
+
+            if terrain_height < 0.0 {
+                terrain_height = 0.0;
+                slope *= -1.0;
+            }
+            heights[x] = terrain_height;
+        }
+
+        Terrain { heights }
+    }
+
+    fn heights(&self) -> [f64; 900] {
+        self.heights
     }
 }
 
